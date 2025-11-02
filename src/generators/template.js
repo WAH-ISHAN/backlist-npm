5694f09504821dbf1caa938ce96bb7fb3b82d499
Wah-Ishan <wahasinduishan@gmail.com>
2025-11-02 15:13:35 +0530

    5.0.6

--- src/generators/template.js
+++ src/generators/template.js
@@ -1,11 +1,17 @@
-const ejs = require('ejs');
 const fs = require('fs-extra');
+const ejs = require('ejs');
 const path = require('path');
 
-async function renderAndWrite(templatePath, destinationPath, data) {
-  const template = await fs.readFile(templatePath, 'utf-8');
-  const content = ejs.render(template, data);
-  await fs.outputFile(destinationPath, content);
+async function renderAndWrite(templatePath, outPath, data) {
+  try {
+    const tpl = await fs.readFile(templatePath, 'utf-8');
+    const code = ejs.render(tpl, data || {}, { filename: templatePath }); // filename helps with EJS errors
+    await fs.outputFile(outPath, code);
+  } catch (err) {
+    console.error('EJS render failed for:', templatePath);
+    console.error('Data keys:', Object.keys(data || {}));
+    throw err;
+  }
 }
 
 function getTemplatePath(subpath) {
